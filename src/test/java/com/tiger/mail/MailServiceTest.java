a56a5227ef06740e501945405e64ca06bb7813c4
tiger <153542758@qq.com>
2017-09-25 11:09:02 +0800

    ...

--- src/test/java/com/tiger/mail/MailServiceTest.java
+++ src/test/java/com/tiger/mail/MailServiceTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import freemarker.template.Configuration;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MailServiceTest {
